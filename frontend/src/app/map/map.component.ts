a4eb9c4c5c71a924abfe9bd934f7f0ef23e6c838
gazawayj <gazaway85@gmail.com>
2025-12-24 13:45:44 -0700

    Angular magic

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -6,12 +6,13 @@ import XYZ from 'ol/source/XYZ';
 
 @Component({
   selector: 'app-map',
+  standalone: true,
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     new Map({
       target: 'map',
       layers: [
