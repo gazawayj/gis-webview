49e7df3a54cc8be81ebf621738ef1a5fd96f2ad6
gazawayj <gazaway85@gmail.com>
2025-12-24 11:14:01 -0700

    initial push

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import mola
+
+app = FastAPI(title="GIS API")
+
+app.include_router(mola.router, prefix="/mola")
+
+@app.get("/")
+def root():
+    return {"status": "API running"}
