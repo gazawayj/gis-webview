b7e18f9a51853ed5488e16b2c625f1cddd1b5305
gazawayj <gazaway85@gmail.com>
2026-01-29 09:33:14 -0700

    update mars tileset address

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -224,7 +224,7 @@ export class MapComponent implements AfterViewInit {
       earth:
         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
       mars:
-        'https://mars-gis.netlify.app/tiles/{z}/{x}/{y}.png',
+        'https://mars-gis.netlify.app{z}/{x}/{y}.png',
       moon:
         'https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png'
     };
