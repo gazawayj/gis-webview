830b27619604838dd02ead647d7620a1ca58774e
gazawayj <gazaway85@gmail.com>
2026-01-29 16:06:36 -0700

    missed a bracket in eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -12,7 +12,8 @@ module.exports = defineConfig([
       "dist/", 
       "README.md", 
       "**/README.md",
-      ".eslintignore" 
+      ".eslintignore"
+    ],
   },
   {
     files: ["**/*.ts"],
