2ba5da6a3ed0520c01af74b511e1df700041b2ad
gazawayj <gazaway85@gmail.com>
2026-01-29 15:34:48 -0700

    test-setup.ts update for vi

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -1,10 +1,22 @@
 import 'zone.js';
 import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+// --- CHANGE THESE IMPORTS ---
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+import { vi } from 'vitest';
 
-class ResizeObserverMock {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
+// THIS MUST RUN BEFORE ANY TESTS
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting()
+);
 
-global.ResizeObserver = ResizeObserverMock;
+// Polyfill ResizeObserver for OpenLayers
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
