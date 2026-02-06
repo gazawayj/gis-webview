8e07ee1c8e6740aad3732bd1ec9fbb460b3d3fa7
gazawayj <gazaway85@gmail.com>
2026-02-06 13:36:17 -0700

    remove backend, migrate to simple to be expanded upon

--- backend/app/db.py
+++
@@ -1,9 +0,0 @@
-def get_mola_features():
-    """
-    Returns sample MOLA features for GIS testing.
-    """
-    return [{
-        "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": [0, 0]},
-        "properties": {"name": "Prime Meridian Center"}
-    }]
