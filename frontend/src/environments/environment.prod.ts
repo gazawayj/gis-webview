24fe750d234b610a65ac65de9052b01b43094547
gazawayj <gazaway85@gmail.com>
2026-02-09 10:38:35 -0700

    update prod command line environment to supposed building in production mode

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  backendUrl: 'https://gis-webview.onrender.com'
+};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
   production: false,
-  get backendUrl(): string {
-    // If running in production (built with --configuration=production), use Render URL
-    // Otherwise, default to localhost for dev
-    return this.production 
-      ? 'https://gis-webview.onrender.com' 
-      : 'http://localhost:3000';
-  }
+  backendUrl: 'http://localhost:3000'
 };
