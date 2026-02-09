c32ccfd6ed3dd2eee3e5a53bca466a02c6ae8f6f
gazawayj <gazaway85@gmail.com>
2026-02-09 11:51:18 -0700

    update map component for deploy push

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -11,6 +11,7 @@ import {
   PLATFORM_ID
 } from '@angular/core';
 
+
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DragDropModule, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
