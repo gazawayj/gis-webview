9e2b2eed27f4c008cd6d9f26d2ac955a9d96e0ec
gazawayj <gazaway85@gmail.com>
2026-02-26 11:11:15 -0700

    Fix icon not showing angular error

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,23 +1,31 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { AppComponent } from './app/app.component';
 import proj4 from 'proj4';
-import { AppModule } from './app/app.module';
 import { register } from 'ol/proj/proj4';
-import { enableProdMode } from '@angular/core';
 
-// Register the IAU codes for Mars (MOLA) and Moon (LROC)
+// Register Mars and Moon IAU codes
 proj4.defs('IAU:49900', '+proj=longlat +a=3396190 +b=3376200 +no_defs +type=crs'); // Mars
 proj4.defs('IAU:30100', '+proj=longlat +a=1737400 +b=1737400 +no_defs +type=crs'); // Moon
-
 register(proj4);
 
-// Bootstrap the Standalone Application using the correct method
 if (environment.production) {
   enableProdMode();
 }
 
-// Bootstrap the Angular AppModule
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
+bootstrapApplication(AppComponent, {
+  providers: [
+    // Import modules for standalone components here
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      FormsModule,
+      DragDropModule,
+      OverlayModule
+    )
+  ]
+}).catch(err => console.error(err));
